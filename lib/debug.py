c98434f2592c626bf069d5817cc48b6317fdd2b1
Joyce Njoroge <joycewambuku33@gmail.com>
2023-06-04 16:21:50 +0300

    complete

--- lib/debug.py
+++ lib/debug.py
@@ -14,6 +14,15 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+  
+    botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+    session.add(botw)
+    session.commit()
 
+    ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+    mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+    session.bulk_save_objects([ffvii, mk8])
+    session.commit()
 
+    fake = Faker()
     import ipdb; ipdb.set_trace()
